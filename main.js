82c08c59186ce1ad2d3655e3862bdd3e9e4c727c
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-08-06 14:35:56 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+function App() {
+    const [data, setData] = React.useState(null);
+    const [loaded, setLoaded] = React.useState(false);
+    const [filter, setFilter] = React.useState('');
+  
+    React.useEffect(() => {
+      async function getData() {
+        const response = await fetch('./movies.json');
+        const json = await response.json();
+        setData(json);
+        setLoaded(true);
+      }
+      getData();
+    }, []);
+    console.log('loaded:', loaded, 'data:', data);
+  
+    return (
+      <>
+      <div className="input-group justify-content-center mb-3 sticky-top">
+          <input
+           type="text" 
+           className="form-control search" 
+           placeholder="Search by movie name" 
+           onKeyPress={(event)=>{
+            if (event.charCode === 13) {
+              console.log("HERE YOU HAVE ACCESS TO YOUR DATA")
+              console.log(filter)
+              console.log("------------")
+              setFilter("")
+          }
+           }}            
+           value={filter} onChange={(e) => {
+             setFilter(e.target.value)}} />
+      </div>
+
+        <div className="container">          
+          <div className="col-sm">
+            {loaded &&
+              data.movies.map((movie, i) => (
+                <Movie
+                  key={i}
+                  title={movie.title}
+                  showTime={movie.showTime}
+                  screen={movie.screen}
+                  row={movie.row}
+                  seat={movie.seat}
+                  imageUrl={movie.imageUrl}
+                />
+                
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
+  
